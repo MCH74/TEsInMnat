ffcb6116cf2cbc84dd8f6c0dac9aca82b6615d21
Frederik Post <74302426+freddyomics@users.noreply.github.com>
2022-09-02 09:53:53 +0200

    Update fig4_Piwi.R

--- fig4_Piwi.R
+++ fig4_Piwi.R
@@ -1,11 +1,13 @@
 library(pheatmap)
+library(RColorBrewer)
 library(viridis)
+library(tidyverse)
 
-df<- as.data.frame(read.table("piwi.txt",sep="\t",header = T))
+df<- as.data.frame(read.table("piwi_update.txt",sep="\t",header = T))
 rownames(df)<-df$gene_id
 df$gene_id<-NULL
 
 hmcol <- viridis(10)
-pdf("heatmap_piwi.pdf",width=6, height=4)
-pheatmap(as.matrix(df),treeheight_row = 0,Rowv=FALSE,col=hmcol,mar=c(10,2),cexCol=0.5,legend_labels="score",cluster_cols = F,show_rownames=T)
+pdf("heatmap_ago_aub.pdf",width=6, height=4)
+pheatmap(as.matrix(df),treeheight_row = 15,treeheight_col=15,Rowv=FALSE,col=hmcol,mar=c(10,2),cexCol=0.5,legend_labels="score",cluster_cols = F,show_rownames=T)
 dev.off()
