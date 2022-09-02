43d3a30939cc00ce1668b829a5e87ee6de3277ed
Frederik Post <74302426+freddyomics@users.noreply.github.com>
2022-09-02 09:43:45 +0200

    Update fig2_cluster_TE_class_distance.R

--- fig2_cluster_TE_class_distance.R
+++ fig2_cluster_TE_class_distance.R
@@ -1,10 +1,15 @@
+library(ggplot2)
+library(dplyr)
+library(ggokabeito)
+library(viridis)
+
 #bar chart TE class; castes
 df <-as.data.frame(read.table("TE_families_caste.txt", sep = "\t", header =TRUE))
 svg(filename="TE_families_caste.svg",width = 9, height = 4)
-ggplot(df, aes(x=factor(Caste,levels = c("Worker","Q0","Q1","Q2","Q3","Q4","King")),y=Number, fill = factor(Element,levels = c("LINE","Other","DNA","LTR","RC")))) +
+ggplot(df, aes(x=factor(Caste,levels=c("FW","Q0m","Q3m","Q9m","Q31m","Q20y","K20y")),y=Number, fill = factor(Element,levels = c("LINE","Other","DNA","LTR","RC")))) +
   geom_bar(position="dodge", stat="identity") +
   geom_text(label=df$Number, position=position_dodge(width=0.9), vjust=-0.25)+
-  scale_fill_brewer(palette="RdYlGn",name="Element")+
+  scale_fill_viridis()+
   xlab("Caste")+
   theme_classic()+
   theme(axis.text.y= element_blank(),
@@ -14,10 +19,10 @@ dev.off()
 #Bar chart TE Class; Heatmap cluster
 df <-as.data.frame(read.table("Heatmap_cluster_annotation_manhattan.txt", sep = "\t", header =TRUE))
 svg(filename="TE_families_cluster_manhattan.svg",width = 12, height = 4)
-ggplot(df, aes(x=factor(Caste,levels = c("Worker","Q0","Q1","Q2","Q3","Q4","King")),y=Number, fill = factor(Element,levels = c("LINE","Other","DNA","LTR","RC")))) +
+ggplot(df, aes(x=factor(Caste,levels=c("FW","Q0m","Q3m","Q9m","Q31m","Q20y","K20y")),y=Number, fill = factor(Element,levels = c("LINE","Other","DNA","LTR","RC")))) +
   geom_bar(position="dodge", stat="identity") +
   geom_text(label=df$Number, position=position_dodge(width=0.9), vjust=-0.25)+
-  scale_fill_brewer(palette="RdYlGn",name="Element")+
+  scale_fill_viridis()+
   xlab("Caste")+
   theme_classic()+
   theme(axis.text.y= element_blank(),
@@ -30,21 +35,15 @@ dev.off()
         plot.title = element_text(hjust = 0.5, color = "#666666"))
 
 #Heatmap cluster distance plot
-library(ggplot2)
-library(dplyr)
-library(viridis)
 df <-as.data.frame(read.table("heatmap_cluster_gene_ID_update.txt", sep = "\t", header =TRUE))
 df <- na.omit(df)
-pdf("anova_distance_manhattan_1000kb_update.pdf",height = 2,width = 7)
-ggplot(df,aes(factor(Caste,levels=c("Worker","Q0","Q1","Q2","Q3","Q4","King")), as.numeric(Distance)))+
-  geom_point(aes(color=Distance,fill=Distance),color="black",position="jitter",shape=21,size=3)+
-  scale_fill_viridis(option=1)+
-  ylim(c(-1000000,1000000))+
-  #scale_y_continuous(breaks=c(-1000000,-500000,0,500000,1000000),
-  #                   labels=c("-1000000"="-1000 kb","-500000"="-500 kb","0","500000"="500 kb","1000000" = "1000 kb"))+
+pdf("anova_distance_manhattan_ito_log2_0.01.pdf",height = 2,width = 7)
+ggplot(df[abs(df$Distance)<=1000000,],aes(factor(Caste,levels=c("FW","Q0m","Q3m","Q9m","Q31m","Q20y","K20y")),log2(as.numeric(abs(Distance+0.01)))))+
+  geom_boxplot(aes(color=factor(Caste,levels=c("FW","Q0m","Q3m","Q9m","Q31m","Q20y","K20y"))))+
+  scale_color_okabe_ito()+
   theme_minimal()+
-  xlab("Distance")+
-  ylab("Cluster")+
+  xlab("Caste")+
+  ylab(expression(log2("Distance to gene [kb]")))+
   theme(legend.position = "none")
 dev.off()
 
